adf5d56a9b2226dcdb7705bbf5aa273fb3b66bbf
oAnjoPHB <anjosruanp@gmail.com>
2026-05-02 21:06:45 -0300

    fix: source folders organization & feat: add Sign In Use Case in application test

--- test/domain/Email.spec.ts
+++ test/domain/Email.spec.ts
@@ -1,27 +1,5 @@
-export class InvalidEmailError extends Error {
-  constructor(message = 'Invalid Email Provided') {
-    super(message)
-  }
-}
-
-export class Email {
-  constructor(private readonly value: string) {
-    this.value = value.trim().toLowerCase()
-
-    if (!Email.validate(this.value)) {
-      throw new InvalidEmailError()
-    }
-  }
-
-  private static validate(email: string): boolean {
-    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$/
-    return regex.test(email)
-  }
-
-  getValue(): string {
-    return this.value
-  }
-}
+import { Email } from '#src/domain/email'
+import { InvalidEmailError } from '#src/domain/errors/email.error'
 
 describe('Email Entity', () => {
   it.each([
