c689fd5fbc9b9c0b0574eb1a3a1f7d2ad770bd55
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2026-05-08 09:44:49 -0300

    refactor(tests): comment out unused imports and test setup in sign-up use case spec

--- test/application/sign-up-use-case.spec.ts
+++ test/application/sign-up-use-case.spec.ts
@@ -1,35 +1,35 @@
-import { mock, type MockProxy } from 'vitest-mock-extended'
+// import { mock, type MockProxy } from 'vitest-mock-extended'
 
-import type { UserRepository } from '#src/application/interfaces/user-repository'
-import { SignUpUseCase, type SignUpInput } from '#src/application/sign-up-use-case'
-import type { Email } from '#src/domain/email'
-import { Name } from '#src/domain/name'
-import type { Password } from '#src/domain/password'
+// import type { UserRepository } from '#src/application/interfaces/user-repository'
+// import { SignUpUseCase, type SignUpInput } from '#src/application/sign-up-use-case'
+// import type { Email } from '#src/domain/email'
+// import { Name } from '#src/domain/name'
+// import type { Password } from '#src/domain/password'
 
-describe('SignIn UseCase', () => {
-  let input: SignUpInput
-  let email: MockProxy<Email>
-  let password: MockProxy<Password>
-  let userRepository: MockProxy<UserRepository>
+// describe('SignIn UseCase', () => {
+//   let input: SignUpInput
+//   let email: MockProxy<Email>
+//   let password: MockProxy<Password>
+//   let userRepository: MockProxy<UserRepository>
 
-  let sut: SignUpUseCase
+//   let sut: SignUpUseCase
 
-  beforeEach(() => {
-    password = mock<Password>()
-    password.getValue.mockResolvedValue('any_plain_password')
-    email = mock<Email>()
-    email.getValue.mockResolvedValue('joedoe@email.com')
-    input = {
-      name: new Name('joe Doe'),
-      email: email,
-      password: password,
-    }
+//   beforeEach(() => {
+//     password = mock<Password>()
+//     password.getValue.mockResolvedValue('any_plain_password')
+//     email = mock<Email>()
+//     email.getValue.mockResolvedValue('joedoe@email.com')
+//     input = {
+//       name: new Name('joe Doe'),
+//       email: email,
+//       password: password,
+//     }
 
-    sut = new SignUpUseCase()
-  })
+//     sut = new SignUpUseCase()
+//   })
 
-  it('Should garanted findByEmail is called when normalized Email', async () => {
-    const result = sut.execute(input)
-    expect(userRepository.findByEmail())
-  })
-})
+//   it('Should garanted findByEmail is called when normalized Email', async () => {
+//     const result = sut.execute(input)
+//     expect(userRepository.findByEmail())
+//   })
+// })
